c9f34594cc3083426a5195ac5ee3bf1a7c878055
onyxsec <etherealsec@gmail.com>
2022-07-09 12:11:40 -0700

    remove config

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-    METAMASK_VERSION: 'v10.15.0',
-    METAMASK_MNEMONIC_PHRASE: '',
-    WALLET_ADDRESS: '0x54eC9B9229e6D7F1a13De95CD38bD2BAf5A0C31F',
-    INFURA: 'https://rinkeby.infura.io/v3/cb3a3e085ac34904aa8d12abdf0a72de',
-}
