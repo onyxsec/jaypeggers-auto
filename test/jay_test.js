d6f2675fff3b5edd0dd2d6b11ad8384e1d967cb6
onyxsec <etherealsec@gmail.com>
2022-07-10 07:19:29 -0700

    use .then() to chain subtasks

--- test/jay_test.js
+++ test/jay_test.js
@@ -32,11 +32,12 @@
   // Close base tab
   await tabs[0].close()
 
-  // NOTICE: metamask.switchNetwork() not working, manually change to Rinkeby
+  // NOTICE: metamask.switchNetwork() not working in dappeteer, manually change to Rinkeby
   await tabs[1].bringToFront()
   await tabs[1].click(constants.MM_NETWORK_DISPLAY)
-  const networks = await tabs[1].$$(constants.MM_NETWORK_OPTIONS)
-  await networks[3].click()
+  await tabs[1].$$(constants.MM_NETWORK_OPTIONS).then( async (networks) => {
+    await networks[3].click()
+  })
 
   // Connect wallet
   await page.bringToFront()
@@ -45,13 +46,13 @@
   await metamask.approve()
   await page.bringToFront()
 
-  // Setup Web3
   web3 = new Web3(config.INFURA)
  })
  
  beforeEach(async () => {
-  const rawStartingBalance = await web3.eth.getBalance(config.WALLET_ADDRESS)
-  startingEthBalance = web3.utils.fromWei(rawStartingBalance, 'ether')
+  await web3.eth.getBalance(config.WALLET_ADDRESS).then((rawStartingBalance) => {
+    startingEthBalance = web3.utils.fromWei(rawStartingBalance, 'ether')
+  })
   console.log("Current ETH Balance: " + startingEthBalance)
 
   if (startingEthBalance < 0.10) {
@@ -64,33 +65,37 @@
     const convertAmt = 0.001
 
     await page.bringToFront()
-    const menuItems = await page.$$(constants.MENU_ITEMS)
-    await menuItems[5].click()
+    await page.$$(constants.MENU_ITEMS).then( async (menuItems) => {
+      await menuItems[5].click()
+    })
+    
+    await page.focus('div.MuiBox-root.css-0 > div > div > div > div:nth-child(1) > div > input').then(() => {
+      page.keyboard.type(convertAmt.toString())
+    })
+    
+    await delay(6000) // TODO: btn needs to convert to clickable 'Buy', waitForXPath doesn't help
+    await page.$x("//button[contains(text(), 'Buy')]").then( async (elements) => {
+      await elements[0].click()
+    })
 
-    await page.focus('div.MuiBox-root.css-0 > div > div > div > div:nth-child(1) > div > input')
-    await page.keyboard.type(convertAmt.toString())
-    await delay(6000) // btn needs to convert to clickable 'Buy'
-
-    const elements = await page.$x('//*[@id="root"]/div/div[2]/div[2]/div/div/button')
-    await elements[0].click()
-    await delay(6000) // wait for mm confirm
+    await delay(6000) // TODO: wait for mm confirm btn, not sure how
     await metamask.confirmTransaction() 
-    await delay(30000) // wait for tx
+    await delay(30000) // TODO: wait for tx, maybe stick in polling loop
 
-    const rawBalance = await web3.eth.getBalance(config.WALLET_ADDRESS)
-    const currentEthBalance = web3.utils.fromWei(rawBalance, 'ether')
-
-    // TODO take tx cost into account or approx range or log for visual compare or something...
-    expect(currentEthBalance).to.eql(startingEthBalance - convertAmt)
+    await web3.eth.getBalance(config.WALLET_ADDRESS).then((rawBalance) => {
+      return web3.utils.fromWei(rawBalance, 'ether')
+    })
+    .then((currentEthBalance) => {
+      // TODO: take tx cost into account or approx range or log for visual compare or something...
+      expect(currentEthBalance).to.eql(startingEthBalance - convertAmt)
+    })
   })
  })
  
- after(async () => {
+ after( async () => {
    await browser.close()
  })
 
- // Desc: general wait fnc
- // Usage: await delay(2000)
 function delay(time_in_ms) {
   return new Promise(function(resolve) { 
       setTimeout(resolve, time_in_ms)
